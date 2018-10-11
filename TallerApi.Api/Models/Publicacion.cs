06a7a2063b6b77d3fe9251f1494f5a23d9bb74ec
Sara Calle <correoofi@dominio.com>
2018-10-10 20:46:30 -0500

    creacion de model/publicacion en TallerApi.Api

---
+++ TallerApi.Api/Models/Publicacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TallerApi.Api.Models
+{
+    public class Publicacion
+    {
+        public int Id { get; set; }
+
+        public string Usuario { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public int MeGusta { get; set; }
+
+        public int MeDisgusta { get; set; }
+
+        public int VecesCompartido { get; set; }
+
+        public bool EsPrivada { get; set; }
+    }
+}
---
+++ TallerApi.Api/Models/TallerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TallerApi.Api.Models
+{
+    public class TallerContext : DbContext
+    {
+        public TallerContext() : base("TallerApiConnection")
+        {
+
+        }
+
+        public DbSet<Publicacion> Publicacion { get; set; }
+    }
+}
+
